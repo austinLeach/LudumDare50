e8eac598a4f24bfe7381fd09242bb87086d8830f
Austin Leach <austinLeach850@gmail.com>
2022-04-02 14:10:31 -0400

    main menu

---
+++ Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   // public AudioSource audio;
+    void Start() {
+   //     audio.time = GlobalVariables.timeInAudio;
+    }
+    public void PlayGame() {
+        // GlobalVariables.timeInAudio = audio.time;
+        SceneManager.LoadScene("Austin");
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
